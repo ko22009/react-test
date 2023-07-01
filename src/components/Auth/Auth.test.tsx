c6330a9bd9099b2f7a777460da98d9e5282c4b7e
Кутлиахметов Руслан Шамилевич <rkutliakhmetov@phoenixit.ru>
2023-07-01 21:17:11 +0500

    add test for auth + add chakra-ui

---
+++ src/components/Auth/Auth.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Auth } from "./Auth";
+
+it("should error when username input is empty", async () => {
+  render(<Auth />);
+  
+  const buttonLogIn = screen.getByRole("button", { name: /login/ });
+
+  userEvent.click(buttonLogIn);
+
+  expect(await screen.findByText(/This is required/)).toBeInTheDocument();
+});
+
+it("should error when username input is less 6 chars", async () => {
+  render(<Auth />);
+  const username = screen.getByLabelText(/Username/);
+  const buttonLogIn = screen.getByRole("button", { name: /login/ });
+
+  userEvent.type(username, "te");
+  userEvent.click(buttonLogIn);
+
+  expect(await screen.findByText(/length should be 6/)).toBeInTheDocument();
+});
+
+it("should error when username input is not latins or numbers", async () => {
+  render(<Auth />);
+
+  userEvent.type(screen.getByLabelText(/Username/), "выуыфввфы!");
+  userEvent.click(screen.getByRole("button", { name: /login/ }));
+
+  expect(
+    await screen.findByText(/only latins and numbers/)
+  ).toBeInTheDocument();
+});
---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,47 @@
+import { useForm } from 'react-hook-form';
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
+
+interface LoginForm {
+  username: string;
+}
+
+export const Auth = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginForm>()
+
+  function onSubmit(values: LoginForm) {
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.username}>
+        <FormLabel>Username
+        <Input
+          {...register('username', {
+            required: 'This is required',
+            minLength: { value: 6, message: 'Minimum length should be 6' },
+            pattern: {
+              value: /^[\w.-]{0,19}[0-9a-zA-Z]$/,
+              message: 'only latins and numbers'
+            }
+          })}
+        /></FormLabel>
+        <FormErrorMessage>
+          {errors.username && errors.username.message}
+        </FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+        login
+      </Button>
+    </form>
+  )
+}
